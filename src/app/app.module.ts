ef107d3e2586d12159134119fa3de7f597277d7f
Diego Ezquerro Calvo <diegoezquerro@gmail.com>
2021-06-22 16:02:55 +0200

    first student version

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,17 +5,20 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FooComponent } from './components/foo/foo.component';
 import { FilmService } from './services/film.service';
+import { StudentComponent } from './components/student/student.component';
+import { StudentService } from './services/student.service';
 
 @NgModule({
   declarations: [
     AppComponent,
-    FooComponent
+    FooComponent,
+    StudentComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [FilmService],
+  providers: [FilmService, StudentService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
