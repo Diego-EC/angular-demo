e2bc47d0d2746797bd6ee6d58b8e9e0baf2d11f7
Diego Ezquerro Calvo <diegoezquerro@gmail.com>
2021-06-24 17:56:26 +0200

    added a login component and routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,21 +6,30 @@ import { AppComponent } from './components/app/app.component';
 import { StudentComponent } from './components/student/student.component';
 import { StudentService } from './services/student.service';
 import { StudentsComponent } from './components/students/students.component';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 import { FlexLayoutModule } from '@angular/flex-layout';
+import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+
+const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: 'students', component: StudentsComponent },
+  { path: '',   redirectTo: '/students', pathMatch: 'full' },
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     StudentComponent,
-    StudentsComponent
+    StudentsComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule,
     FlexLayoutModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [StudentService],
   bootstrap: [AppComponent]
